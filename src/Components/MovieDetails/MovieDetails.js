3b5ba5f5c938d89be6030286478a8afadecce458
Shannon Lichtenwalter <shannon.lichtenwalter@gmail.com>
2020-03-30 22:29:08 -0600

    flexbox adjustments

--- src/Components/MovieDetails/MovieDetails.js
+++ src/Components/MovieDetails/MovieDetails.js
@@ -2,14 +2,15 @@ import React from 'react';
 import { withRouter } from 'react-router-dom';
 import GetMovieLists from '../../Services/getMovieLists'
 import Movie from '../Movie/Movie';
+import './MovieDetails.css';
 
-class MovieDetails extends React.Component{
+class MovieDetails extends React.Component {
   state = {
     similarFilms: null
   }
 
   componentDidMount = () => {
-    if(!this.props.movieDetails){
+    if (!this.props.movieDetails) {
       this.props.history.push('/');
     };
   }
@@ -21,35 +22,43 @@ class MovieDetails extends React.Component{
   componentDidMount = () => {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
-    
-    GetMovieLists.getSimilarFilms(this.props.movieDetails.id)
-      .then(res => {
-        this.setState({
-          similarFilms: res
-        })
-      }).catch(e => this.props.setError(e))
+
+    if (this.props.movieDetails) {
+      GetMovieLists.getSimilarFilms(this.props.movieDetails.id)
+        .then(res => {
+          this.setState({
+            similarFilms: res
+          })
+        }).catch(e => this.props.setError(e))
+    }
   }
 
-  render(){
-    return(
+  render() {
+    return (
       <section>
-        {!this.props.movieDetails 
-        ? this.returnHome()
-        : <div>
-          <button onClick={()=>this.props.history.goBack()}>back</button>
-            {this.props.movieDetails.poster_path 
-              ? <img src={`https://image.tmdb.org/t/p/w200/${this.props.movieDetails.poster_path}`} alt='movie poster'/>
-              : <div className='poster'>No Poster Data Available</div>
-            }
-            <h2>{this.props.movieDetails.title}</h2>
-            <h3>Release Date: {new Date(this.props.movieDetails.release_date).toLocaleDateString()}</h3>
-            <h4>Average Votes: {this.props.movieDetails.vote_average}/10</h4>
-            <h4>Original Title: {this.props.movieDetails.original_title}</h4>
-            <p>{this.props.movieDetails.overview}</p>
+        {!this.props.movieDetails
+          ? this.returnHome()
+          : <div>
+              <div className='detailedPage'>
+                <div>
+                  <button onClick={() => this.props.history.goBack()}>back</button>
+                  {this.props.movieDetails.poster_path
+                    ? <img className='posterImage' src={`https://image.tmdb.org/t/p/w300/${this.props.movieDetails.poster_path}`} alt='movie poster' />
+                    : <div className='poster'>No Poster Data Available</div>
+                  }
+                </div>
+                <div>
+                  <h2>{this.props.movieDetails.title}</h2>
+                  <h3>Release Date: {new Date(this.props.movieDetails.release_date).toLocaleDateString()}</h3>
+                  <h4>Average Votes: {this.props.movieDetails.vote_average}/10</h4>
+                  <h4>Original Title: {this.props.movieDetails.original_title}</h4>
+                  <p>{this.props.movieDetails.overview}</p>
+                </div>
+              </div>
             Similar Movies:
             <ul className='movies'>
               {this.state.similarFilms && this.state.similarFilms.results.map((movie, index) => {
-                return <Movie key={index} data={movie} showDetails={this.props.showDetails}/>
+                return <Movie key={index} data={movie} showDetails={this.props.showDetails} />
               })}
             </ul>
           </div>
